4396f4dcb20a7a9739c94e8176ba375ecc07a51e
Grek <101201468+Grek264@users.noreply.github.com>
2023-05-07 13:56:33 +0200

    функция match count завершена(нужно прооизвести потом доп проверку перед запуском) начал делать подсчёт аудитов

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ name_file =""
 
 import pandas as pd
 
+import numpy as np
+
+
 @dp.callback_query_handler(text=['pars_chek'])
 async def process_start_command(message: types.Message):
     answer = steel_phoenix_pars(0)
@@ -58,6 +61,8 @@ async def processing_message(message: types.Message):
 
 
 
+
+
 @dp.callback_query_handler(text="audit_processing")
 async def processing_message(message: types.Message):
     await bot.send_message(message.from_user.id,"Пожалуйста укажите название под которым вы хотите сохранить этот файл")
@@ -78,41 +83,49 @@ def save_file (key):
         if (key == "match"):
             count_mach(name_file ,message.from_user.id )
             await bot.send_message(message.from_user.id,"обработка файла завершена отправляю вам финальну сводку \n /function_menu")
-            f = open(f'file\match\{name_file}_result.xlsx',"rb")
+            f = open(f'file\\match\\{name_file}_result.xlsx',"rb")
+            await bot.send_document(message.chat.id, f)
+        elif(key == "audit"):
+            count_mach(name_file,message.from_user.id)
+            await bot.send_message(message.from_user.id,"обработка файла завершена отправляю вам финальный результат \n /function_menu")
+            f = open(f'file\\audit\\{name_file}_result.xlsx', "rb")
             await bot.send_document(message.chat.id, f)
 
 
 
+def steel_phoenix_pars(audit , id):
+    file = pd.read_excel(f'file\\audit\\{name_file}.xlsx')
+    file.head()
 
+    url = 'https://steel-phoenix.ru/sc/report/auditcheck.php'
+    login = 'IvanFlay3r'
+    password = 'U4pcWqH4n'
 
-def steel_phoenix_pars(audit):
-    link = 'https://steel-phoenix.ru/sc/report/auditcheck.php'
-    session =requests.Session()
-    user = fake_useragent.UserAgent().random
-    log_data ={
-        "username": "IvanFlay3r",
-        "password": "U4pcWqH4n",
-        "g - recaptcha - response": "03",
-        # AKH6MRFybb - cQuItAjK0kAHxVN1js7pJnirZ_8j9ykNwFzRg3sZcWWyLwv9JAL8Cgi2FzO1o_i2thuvygSTDuXWILgWfv3uihkuKe9DPTVnprQQYyVABK_d2 - y5NtKYLll_Vl_bG4BA1_x - zh8Sz0abUtKRgueEsJHZuqDT9r0roBB5hhpES7_ugO6MUzPXEw4KaVrItgnlk7096zr7XEeZuynxvTdVPVN4pLTsfu98j_JRSWv - Ugrd - UGkmxPM8FTKsr6cUlkWETOC0m6JReo5s1PGB6S0OPtMZw4d_0ug7GSbKZZG4NB7J8xlic0_o_eh4_p6pJrFEiV - -y5smWMo_KaSnunUxdGGUUF4rBiTw5kIAv7AGigCEWz23MuJzOUi4oret5QbqkCBQbIuqAfQMBzzKXGA8aKgsxfTwNgyZ - m9oZWqzb66_0jdZxTJsxmKsRDWX4RzJMF7MC - bij3FEij9UAoUbrZcNFcWnrlpSYQh - SYTuwIDZVF_wfOAuQsNeKXSWhD - S
-            "lang": "en",
-    "g - recaptcha - response": "03",
-    # AKH6MRFybb - cQuItAjK0kAHxVN1js7pJnirZ_8j9ykNwFzRg3sZcWWyLwv9JAL8Cgi2FzO1o_i2thuvygSTDuXWILgWfv3uihkuKe9DPTVnprQQYyVABK_d2 - y5NtKYLll_Vl_bG4BA1_x - zh8Sz0abUtKRgueEsJHZuqDT9r0roBB5hhpES7_ugO6MUzPXEw4KaVrItgnlk7096zr7XEeZuynxvTdVPVN4pLTsfu98j_JRSWv - Ugrd - UGkmxPM8FTKsr6cUlkWETOC0m6JReo5s1PGB6S0OPtMZw4d_0ug7GSbKZZG4NB7J8xlic0_o_eh4_p6pJrFEiV - -y5smWMo_KaSnunUxdGGUUF4rBiTw5kIAv7AGigCEWz23MuJzOUi4oret5QbqkCBQbIuqAfQMBzzKXGA8aKgsxfTwNgyZ - m9oZWqzb66_0jdZxTJsxmKsRDWX4RzJMF7MC - bij3FEij9UAoUbrZcNFcWnrlpSYQh - SYTuwIDZVF_wfOAuQsNeKXSWhD - S
-    }
-    header = {
-        'user-agent': user
-    }
-    respose = session.post(link,data=log_data,headers= header ).text
-    return respose
+    session = requests.Session()
+    session.auth = (login, password)
+
+    response = session.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Найдите поле с ссылкой и вставьте передаваемый текст
+    link_field = soup.find('input', {'name': 'link'})
+    link_field['value'] = 'https://example.com'
+
+    # Отправьте форму
+    response = session.post(url, data=soup.select_one('form').attrs['data-ajax-form'])
+    print(response.text)
+    return response
 
 def count_mach(name_file , id ):
     # cols_user_base = [1, 3, 4, 9, 11]
     file = pd.read_excel(f'file\\match\\{name_file}.xlsx')
     file.head()
-    result = pd.DataFrame({'Name': [], 'Games': [], 'Date': []})
-    df = pd.DataFrame(result, columns=['Name', 'Games', 'Date'])
+    result = pd.DataFrame({ 'Date/name': []})
+    df = pd.DataFrame(result, columns=[ 'Date/name'])
     result = pd.concat([result, df], ignore_index=True, sort=False)
-    result.to_excel(f'file\match\{name_file}_result.xlsx')
-    result = pd.read_excel(f'file\match\{name_file}_result.xlsx')
+    result.to_excel(f'file\\match\\{name_file}_result.xlsx')
+    result = pd.read_excel(f'file\\match\\{name_file}_result.xlsx')
     result.head()
     name = file['Alias'].tolist()
     Start_Date = (file['Period'].tolist())
@@ -122,67 +135,121 @@ def count_mach(name_file , id ):
     percent = lengh / 100
     for nuber in range(0, lengh):
         print(f'{nuber / percent} %')
-        Name_result = result['Name'].tolist()
-        Games_result = result['Games'].tolist()
-        Date = result['Date'].tolist()
+        print(nuber)
+        if str(name[nuber]) ==('Freedoom'):
+            print("fak")
+        Name_result = (result.columns.ravel())
+        Date = result['Date/name'].tolist()
 
-        if (name[nuber] in Name_result) == False:
-            if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG':
-                count =  3 * Games[nuber]
-            elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX' :
-                count = 3 * Games[nuber]
-            elif Speed[nuber] == 'Normal':
-                count = 1 * Games[nuber]
-            elif Speed[nuber] == 'Nitro':
-                count =  Games[nuber] / 3
-                # str(Start_Date[nuber].strftime("%m %Y"))
-            new_player = ({'Name': [name[nuber]], 'Games':[count], 'Date':[(str(Start_Date[nuber])[0:7])]})
-            df = pd.DataFrame(new_player, columns=['Name','Games','Date'])
-            result = pd.concat([result, df], ignore_index=True, sort=False)
-            result.to_excel(f'file\match\{name_file}_result.xlsx')
+        # if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG' or Speed[nuber] == 'Nitro':
+        #     count = 3 * Games[nuber]
+        # elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
+        #     count = 3 * Games[nuber]
+        # elif Speed[nuber] == 'Normal':
+        #     count = 1 * Games[nuber]
+
+        if (str(name[nuber]) in str(Name_result)) == False:
+            if ((str(Start_Date[nuber])[0:7]) in Date):
+                boolean_variable = -1
+                while boolean_variable == -1:
+                    index = 0
+                    for index in range(len(Date)):
+                        if (str(Start_Date[nuber])[0:7]) == (Date[index]):
+                            boolean_variable = index
+                    if boolean_variable != -1:
+                        if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG' or Speed[nuber] == 'Nitro':
+                            count = 3 * Games[nuber]
+                        elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
+                            count = 3 * Games[nuber]
+                        elif Speed[nuber] == 'Normal':
+                            count = 1 * Games[nuber]
+                        isNan = True
+                        df = pd.DataFrame( columns=['Date/name', f'{[name[nuber]]}'])
+                        result = pd.concat([result, df], ignore_index=True, sort=False)
+                        if isNan == np.isnan(result.at[boolean_variable, f'{[name[nuber]]}']): #Хрень
+                            result.at[boolean_variable, f'{[name[nuber]]}'] = count
+                        else:
+                            result.at[boolean_variable, f'{[name[nuber]]}'] += count
+
+                        result.to_excel(f'file\\match\\{name_file}_result.xlsx')
+                    else:
+                        boolean_variable = 0
+                        if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG':
+                            count = 3 * Games[nuber]
+                        elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
+                            count = 3 * Games[nuber]
+                        elif Speed[nuber] == 'Normal':
+                            count = 1 * Games[nuber]
+                        elif Speed[nuber] == 'Nitro':
+                            count = Games[nuber] / 3
+                        new_player = ({ 'Date': [(str(Start_Date[nuber])[0:7])],f'{[name[nuber]]}':[count] })
+                        df = pd.DataFrame(new_player, columns=[ 'Date/name' , f'{[name[nuber]]}'])
+                        result = pd.concat([result, df], ignore_index=True, sort=False)
+                        result.to_excel(f'file\\match\\{name_file}_result.xlsx')
+
+            else:
+                if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG':
+                    count = 3 * Games[nuber]
+                elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
+                    count = 3 * Games[nuber]
+                elif Speed[nuber] == 'Normal':
+                    count = 1 * Games[nuber]
+                elif Speed[nuber] == 'Nitro':
+                    count = Games[nuber] / 3
+                new_player = ({'Date/name': [(str(Start_Date[nuber])[0:7])] , f'{[name[nuber]]}':[count]})
+                df = pd.DataFrame(new_player, columns=['Date/name', f'{[name[nuber]]}'])
+                result = pd.concat([result, df], ignore_index=True, sort=False)
+                result.to_excel(f'file\\match\\{name_file}_result.xlsx')
         elif ((str(Start_Date[nuber])[0:7]) in Date):
             boolean_variable = -1
             while boolean_variable == -1:
-                index = 1
-                for index in range(len(Name_result)):
-                    if Name_result[index] == name[nuber] and (str(Start_Date[nuber])[0:7]) == (Date[index]):
+                index = 0
+                for index in range(len(Date)):
+                    if (str(Start_Date[nuber])[0:7]) == (Date[index]):
                         boolean_variable = index
                 if boolean_variable != -1:
-                    if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG' or Speed[nuber] == 'Nitro' :
+                    if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG' or Speed[nuber] == 'Nitro':
                         count = 3 * Games[nuber]
                     elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
                         count = 3 * Games[nuber]
                     elif Speed[nuber] == 'Normal':
                         count = 1 * Games[nuber]
-                    result.at[boolean_variable, 'Games'] = count + Games_result[boolean_variable]
-                    result.to_excel(f'file\match\{name_file}_result.xlsx')
+
+                    df = pd.DataFrame(columns=[ f'{[name[nuber]]}'])
+                    result = pd.concat([result, df], ignore_index=True, sort=False)
+                    isNan = True
+                    if isNan == np.isnan(result.at[boolean_variable, f'{[name[nuber]]}']):  # Хрень
+                        result.at[boolean_variable, f'{[name[nuber]]}'] = count
+                    else:
+                        result.at[boolean_variable, f'{[name[nuber]]}'] += count
+                    result.to_excel(f'file\\match\\{name_file}_result.xlsx')
                 else:
                     boolean_variable = 0
                     if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG':
-                       count = 3 * Games[nuber]
+                        count = 3 * Games[nuber]
                     elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
                         count = 3 * Games[nuber]
                     elif Speed[nuber] == 'Normal':
                         count = 1 * Games[nuber]
                     elif Speed[nuber] == 'Nitro':
                         count = Games[nuber] / 3
-                    new_player = ({'Name': [name[nuber]], 'Games': [count], 'Date': [(str(Start_Date[nuber])[0:7])]})
-                    df = pd.DataFrame(new_player, columns=['Name', 'Games', 'Date'])
+                    new_player = ({f'{[name[nuber]]}':[count]})
+                    df = pd.DataFrame(new_player, columns=[ f'{[name[nuber]]}'])
                     result = pd.concat([result, df], ignore_index=True, sort=False)
-                    result.to_excel(f'file\match\{name_file}_result.xlsx')
+                    result.to_excel(f'file\\match\\{name_file}_result.xlsx')
         else:
             if Speed[nuber] == 'Flash' or Speed[nuber] == 'GG':
-                        count = 3 * Games[nuber]
+                count = 3 * Games[nuber]
             elif Speed[nuber] == 'Winamax' or Speed[nuber] == 'WMX':
-                        count = 3 * Games[nuber]
+                count = 3 * Games[nuber]
             elif Speed[nuber] == 'Normal':
-                        count = 1 * Games[nuber]
+                count = 1 * Games[nuber]
             elif Speed[nuber] == 'Nitro':
                 count = Games[nuber] / 3
-            new_player = ({'Name': [name[nuber]], 'Games': [count], 'Date': [(str(Start_Date[nuber])[0:7])]})
-            df = pd.DataFrame(new_player, columns=['Name', 'Games', 'Date'])
+            new_player = ({'Date/name': [(str(Start_Date[nuber])[0:7])], f'{[name[nuber]]}':[count]})
+            df = pd.DataFrame(new_player, columns=['Date/name', f'{[name[nuber]]}'])
             result = pd.concat([result, df], ignore_index=True, sort=False)
-            result.to_excel(f'file\match\{name_file}_result.xlsx')
+            result.to_excel(f'file\\match\\{name_file}_result.xlsx')
 
 
 if __name__ == '__main__':
